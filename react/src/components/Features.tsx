c0870d6fe818624a6b2367cac0d2033b1c698d39
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-08-11 21:23:46 +0500

    Update Features.tsx

--- react/src/components/Features.tsx
+++ react/src/components/Features.tsx
@@ -4,17 +4,25 @@ import { CiParking1 } from "react-icons/ci";
 import { SlPlane } from "react-icons/sl";
 import { GiBroom } from "react-icons/gi";
 
+const featuresData = [
+  { icon: <IoWifi />, feature: "Free WiFi" },
+  { icon: <BsLamp />, feature: "Fully Furnished" },
+  { icon: <CiParking1 />, feature: "Free Parking" },
+  { icon: <SlPlane />, feature: "Airport Transfers" },
+  { icon: <GiBroom />, feature: "Cleaning Services" },
+]
 
 
 
 const Features = () => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 px-8 md:px-16 lg:px-24 py-2">
-      <h1 className="text-center"><IoWifi /></h1>      
-      <h1 className="text-center"><BsLamp /></h1>      
-      <h1 className="text-center"><CiParking1 /></h1>      
-      <h1 className="text-center"><SlPlane /></h1>      
-      <h1 className="text-center"><GiBroom /></h1>      
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 px-8 md:px-16 lg:px-24 py-4">
+      {featuresData.map((feature, index) => (
+        <div key={index} className="flex flex-col items-center justify-center gap-4 mb-4">
+          <span className="text-4xl font-light">{feature.icon}</span>
+          <p className="text-lg">{feature.feature}</p>
+        </div>        
+      ))}
     </div>
   )
 }
